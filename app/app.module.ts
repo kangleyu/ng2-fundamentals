8ca168caf86d317825a074fba16e5ee8248afd02
Tom Yu <kangleyu@gmail.com>
2017-04-10 06:10:50 +0800

    add session list component, and pass data from parent component to child

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,7 +11,8 @@ import {
   EventRouteActivator,
   EventListResolver,
   EventService,
-  CreateSessionComponent
+  CreateSessionComponent,
+  SessionListComponent
 } from './events/index';
 
 import { EventsAppComponent } from './events-app-component';
@@ -38,7 +39,8 @@ import { appRoutes } from './routes';
     EventDetailsComponent,
     CreateEventComponent,
     Error404Component,
-    CreateSessionComponent
+    CreateSessionComponent,
+    SessionListComponent
   ],
   providers: [
     EventService,
