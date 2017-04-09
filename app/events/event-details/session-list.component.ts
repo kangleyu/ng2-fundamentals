63c0b4da86ed4be9b57af202c0a5cf1f5c956169
Tom Yu <kangleyu@gmail.com>
2017-04-10 07:28:51 +0800

    add sorting function for session list

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -8,11 +8,14 @@ import { ISession } from '../shared/index';
 export class SessionListComponent {
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[] = [];
 
   ngOnChanges() {
     if (this.sessions) {
       this.filterSessions(this.filterBy);
+      console.log(this.sortBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
     }
   }
 
@@ -25,4 +28,14 @@ export class SessionListComponent {
       })
     }
   }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) return 1;
+  else if (s1.name === s2.name) return 0;
+  else return -1;
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
