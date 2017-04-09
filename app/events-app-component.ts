a36774318d4aff9b202ca197f1ab138061cae534
Tom Yu <kangleyu@gmail.com>
2017-04-09 13:10:11 +0800

    add navigator bar component

--- app/events-app-component.ts
+++ app/events-app-component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<events-list></events-list>'
+  templateUrl: 'app/events-app-component.html'
 })
 export class EventsAppComponent {
 
