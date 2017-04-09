ba0b09b2f17749c9e6906688f8e8fcbaa1c4aae3
Tom Yu <kangleyu@gmail.com>
2017-04-09 15:46:32 +0800

    add shared event service and inject for component

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,6 +6,8 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 
+import { EventService } from './events/shared/event.service';
+
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ 
@@ -13,6 +15,9 @@ import { NavBarComponent } from './nav/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
+  ],
+  providers: [
+    EventService
   ],
   bootstrap: [ EventsAppComponent ]
 })
