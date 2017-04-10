484c0da94c8431653d8ab4f1dbfe36169b43749a
Tom Yu <kangleyu@gmail.com>
2017-04-10 21:31:43 +0800

    pass modal element id from outside

--- app/common/modal-trigger.directive.ts
+++ app/common/modal-trigger.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Inject, ElementRef } from '@angular/core';
+import { Directive, OnInit, Inject, ElementRef, Input } from '@angular/core';
 import { JQ_TOKEN } from './index';
 
 @Directive({
@@ -6,6 +6,7 @@ import { JQ_TOKEN } from './index';
 })
 export class ModalTriggerDirective implements OnInit {
   private el: HTMLElement;
+  @Input('modal-trigger') modalId: string;
 
   constructor(
     el: ElementRef,
@@ -15,7 +16,7 @@ export class ModalTriggerDirective implements OnInit {
 
   ngOnInit() {
     this.el.addEventListener('click', e => {
-      this.$('#simple-modal').modal({});
+      this.$(`#${this.modalId}`).modal({});
     })
   }
 }
