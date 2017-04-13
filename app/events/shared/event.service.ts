b03ae145c7209fb0065935fbd2f8b2c6823c34fc
Tom Yu <kangleyu@gmail.com>
2017-04-13 15:11:16 +0800

    update get event to call service, and use resolver instaler canacitve

--- app/events/shared/event.service.ts
+++ app/events/shared/event.service.ts
@@ -15,8 +15,10 @@ export class EventService {
     }).catch(this.handleError);
   }
 
-  getEvent (id: number): IEvent {
-    return EVENTS.find(event => event.id === id);
+  getEvent (id: number): Observable<IEvent> {
+    return this.http.get("/api/events/" + id).map((response: Response) => {
+      return <IEvent>response.json();
+    }).catch(this.handleError);
   }
 
   saveEvent(event) {
--- app/routes.ts
+++ app/routes.ts
@@ -6,6 +6,7 @@ import {
   CreateEventComponent,
   EventRouteActivator,
   EventListResolver,
+  EventResolver,
   CreateSessionComponent
 } from './events/index';
 
@@ -14,7 +15,7 @@ import { Error404Component } from './errors/404.service';
 export const appRoutes: Routes = [
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactiveCreateEvent'] },
   { path: 'events', component: EventsListComponent, resolve: { events: EventListResolver } },
-  { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+  { path: 'events/:id', component: EventDetailsComponent, resolve: { event: EventResolver } },
   { path: 'events/session/new', component: CreateSessionComponent },
   { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
