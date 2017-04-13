0d6762c47d702922d4de0ba9d00bd751e89ac1e5
Tom Yu <kangleyu@gmail.com>
2017-04-13 21:16:22 +0800

    tunning rxjs requests

---
+++ app/rxjs-extensions.ts
@@ -0,0 +1,9 @@
+import "rxjs/add/observable/of";
+import "rxjs/add/observable/throw";
+
+import "rxjs/add/oprator/catch";
+import "rxjs/add/oprator/do";
+import "rxjs/add/oprator/filter";
+import "rxjs/add/oprator/map";
+
+
